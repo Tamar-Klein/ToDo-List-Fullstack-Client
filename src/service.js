4be7d3e7fc33bc2f41bd5f652eaa1edc55c4c002
Tamar-Klein <tk6742866@gmail.com>
2025-12-02 17:28:54 +0200

    content to func commit

--- src/service.js
+++ src/service.js
@@ -1,25 +1,40 @@
 import axios from 'axios';
-const apiUrl =  process.env.REACT_APP_API_URL
+
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export default {
+
+  // שליפת משימות
   getTasks: async () => {
-    const result = await axios.get(`${apiUrl}/tasks`)    
+    const result = await axios.get(`${apiUrl}/tasks`);
     return result.data;
   },
 
-  addTask: async(name)=>{
-    console.log('addTask', name)
-    //TODO
-    return {};
+  // הוספת משימה
+  addTask: async (name) => {
+    console.log("addTask", name);
+    const result = await axios.post(`${apiUrl}/tasks`, {
+      name: name,
+      isComplete: false
+    });
+    return result.data;
   },
 
-  setCompleted: async(id, isComplete)=>{
-    console.log('setCompleted', {id, isComplete})
-    //TODO
-    return {};
+  // שינוי סטטוס משימה
+  setCompleted: async (id, isComplete) => {
+    console.log("setCompleted", { id, isComplete });
+    const result = await axios.put(`${apiUrl}/tasks/${id}`, {
+      id,
+      isComplete,
+      name: undefined // יתעדכן מהשרת אם צריך
+    });
+    return result.data;
   },
 
-  deleteTask:async()=>{
-    console.log('deleteTask')
+  // מחיקת משימה
+  deleteTask: async (id) => {
+    console.log("deleteTask", id);
+    const result = await axios.delete(`${apiUrl}/tasks/${id}`);
+    return result.data;
   }
 };
